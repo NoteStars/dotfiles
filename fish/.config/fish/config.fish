985518302d729ce08ae05d5ebc6660242e30287f
Jonathan Nguyen <jonathannguyen53@gmail.com>
2025-11-26 00:55:12 -0800

    Added z

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,5 +8,9 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 oh-my-posh init fish --config 'https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/velvet.omp.json' | source
 
+#Set up zoxide PATH
+zoxide init fish | source
+
 # --- Set Yazi to use Emacs by default ---
 export EDITOR=emacs
+
